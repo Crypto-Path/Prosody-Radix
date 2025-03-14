3d0df62044a0817b4eb8fa54f53d1c8d1980e9c5
Crypto-Path <cyphe371@gmail.com>
2025-03-14 12:50:22 -0700

    Added resources (font) and updated main.c to compile

--- main.c
+++ main.c
@@ -2,33 +2,29 @@
 #include "clay/clay.h"
 #include "clay/renderers/raylib/clay_renderer_raylib.c"
 
-int main(void) {
-	Clay_Raylib_Initialize(800, 600, "Prosody", FLAG_WINDOW_RESIZABLE);
+const uint32_t FONT_ID_BODY_16 = 0;
 
+void HandleClayErrors(Clay_ErrorData errorData) {
+    printf("%s", errorData.errorText.chars);
+}
+
+int main(void) {
 	uint64_t clayRequireMemory = Clay_MinMemorySize();
-	Clay_Arena clayMemory = (Clay_Arena) {
-		.memory = malloc(clayRequireMemory),
-		.capacity = clayRequireMemory
-	};
-	Clay_Initialize(clayMemory, (Clay_Dimensions) {
-		.width = GetScreenWidth(),
-		.height = GetScreenHeighr()
-	});
+	Clay_Arena clayMemory = (Clay_Arena) { .memory = malloc(clayRequireMemory), .capacity = clayRequireMemory };
+	Clay_Initialize(clayMemory, (Clay_Dimensions) { .width = GetScreenWidth(), .height = GetScreenHeight() / 2},  (Clay_ErrorHandler) { HandleClayErrors });
+	Clay_Raylib_Initialize(800, 600, "Prosody", FLAG_WINDOW_RESIZABLE);
 	
+	Font fonts[1];
+    fonts[FONT_ID_BODY_16] = LoadFontEx("./resources/Roboto-Regular.ttf", 48, 0, 400);
+    SetTextureFilter(fonts[FONT_ID_BODY_16].texture, TEXTURE_FILTER_BILINEAR);
+
 	while (!WindowShouldClose()) {
 		Clay_BeginLayout();
 		
-		CLAY(
-			CLAY_RECTANGLE({
-				.color = { 89, 85, 96, 255 }
-			}),
-			CLAY_LAYOUT({
-				.sizing = {
-					.width = CLAY_SIZING_GROW(),
-					.height = CLAY_SIZING_GROW()
-				}
-			})
-		) {
+		CLAY({ .id = CLAY_ID(" body"),
+			.layout = { .sizing = { .width = CLAY_SIZING_GROW(0), .height = CLAY_SIZING_GROW(0)} },
+			.backgroundColor =  {180, 180, 180, 255}
+		}) {
 			
 		}
 		
@@ -36,7 +32,7 @@ int main(void) {
 		
 		BeginDrawing();
 		ClearBackground(BLACK);
-		Clay_Raylib_Render(renderCommands);
+		Clay_Raylib_Render(renderCommands, fonts);
 		EndDrawing();
 	}
 

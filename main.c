b1eb6b78bbf167dfc7f6eb8be0b4d4e5658d0872
Crypto-Path <cyphe371@gmail.com>
2025-03-15 02:36:29 -0700

    Fixed raylib to clay render order + simple ui set up

--- main.c
+++ main.c
@@ -11,12 +11,13 @@ void HandleClayErrors(Clay_ErrorData errorData) {
 int main(void) {
 	uint64_t clayRequireMemory = Clay_MinMemorySize();
 	Clay_Arena clayMemory = (Clay_Arena) { .memory = malloc(clayRequireMemory), .capacity = clayRequireMemory };
-	Clay_Initialize(clayMemory, (Clay_Dimensions) { .width = GetScreenWidth(), .height = GetScreenHeight() },  (Clay_ErrorHandler) { HandleClayErrors });
 	Clay_Raylib_Initialize(800, 600, "Prosody Clay", FLAG_WINDOW_RESIZABLE);
+	Clay_Initialize(clayMemory, (Clay_Dimensions) { .width = GetScreenWidth(), .height = GetScreenHeight() }, (Clay_ErrorHandler) { HandleClayErrors });
 	
 	Font fonts[1];
 	fonts[FONT_ID_BODY_16] = LoadFontEx("resources/Roboto-Regular.ttf", 48, 0, 400);
 	SetTextureFilter(fonts[FONT_ID_BODY_16].texture, TEXTURE_FILTER_BILINEAR);
+    Clay_SetMeasureTextFunction(Raylib_MeasureText, fonts);
 
 	while (!WindowShouldClose()) {
         Clay_SetLayoutDimensions((Clay_Dimensions) {
@@ -26,11 +27,43 @@ int main(void) {
         
 		Clay_BeginLayout();
 		
-		CLAY({ .id = CLAY_ID(" body"),
-			.layout = { .sizing = { .width = CLAY_SIZING_GROW(0), .height = CLAY_SIZING_GROW(0)} },
-			.backgroundColor =  {180, 180, 180, 255}
-		}) {
-			
+		CLAY({ .id = CLAY_ID("body"),
+			.layout = {
+				.layoutDirection = CLAY_LEFT_TO_RIGHT,
+				.sizing = {
+					.width = CLAY_SIZING_GROW(0),
+					.height = CLAY_SIZING_GROW(0) 
+				}
+			},
+			.backgroundColor = {95, 90, 105, 255}
+		} ) {
+			CLAY({ .id = CLAY_ID(" chartDataContainer"),
+				.layout = {
+					.sizing = {
+						.width = CLAY_SIZING_GROW(0),
+						.height = CLAY_SIZING_GROW(0)
+					}
+				}
+			}) {
+				// Chart Info & stuff
+			}
+			CLAY({ .id = CLAY_ID("chartListContainer"),
+				.layout = {
+					.layoutDirection = CLAY_TOP_TO_BOTTOM,
+					.sizing = {
+						.width = CLAY_SIZING_FIXED(240),
+						.height = CLAY_SIZING_GROW(0)
+					}
+				},
+				.backgroundColor = {185, 180, 195, 200},
+            	.cornerRadius = CLAY_CORNER_RADIUS(8)
+			}) {
+				CLAY_TEXT(CLAY_STRING("Hello World"), CLAY_TEXT_CONFIG({
+                    .fontId = FONT_ID_BODY_16,
+					.fontSize = 24,
+					.textColor = {255, 255, 255, 255} 
+				}));
+			}
 		}
 		
 		Clay_RenderCommandArray renderCommands = Clay_EndLayout();
@@ -41,5 +74,6 @@ int main(void) {
 		EndDrawing();
 	}
 
+	Clay_Raylib_Close();
 	return 0;
 }

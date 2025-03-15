1a49bf38bd7520f1a7dd813650db50b3f6f80c7d
Crypto-Path <cyphe371@gmail.com>
2025-03-14 21:29:27 -0700

    Clay is working 🥳🎉

--- Makefile
+++ Makefile
@@ -8,11 +8,14 @@ OUTPUT = index.html
 # Sources
 SRC = main.c
 
+# Font file embedding
+EMBED_FILES = --embed-file resources/Roboto-Regular.ttf
+
 # Target to build index.html, index.js, and index.wasm
 all: $(OUTPUT)
 
 $(OUTPUT): $(SRC)
-	$(CC) $(SRC) -o $(OUTPUT) $(CFLAGS) $(INCLUDE) $(LIBRARY) -s ERROR_ON_UNDEFINED_SYMBOLS=0
+	$(CC) $(SRC) -o $(OUTPUT) $(CFLAGS) $(INCLUDE) $(LIBRARY) $(EMBED_FILES) -s ERROR_ON_UNDEFINED_SYMBOLS=0
 
 # Clean target to remove build artifacts
 clean:
--- main.c
+++ main.c
@@ -11,14 +11,19 @@ void HandleClayErrors(Clay_ErrorData errorData) {
 int main(void) {
 	uint64_t clayRequireMemory = Clay_MinMemorySize();
 	Clay_Arena clayMemory = (Clay_Arena) { .memory = malloc(clayRequireMemory), .capacity = clayRequireMemory };
-	Clay_Initialize(clayMemory, (Clay_Dimensions) { .width = GetScreenWidth(), .height = GetScreenHeight() / 2},  (Clay_ErrorHandler) { HandleClayErrors });
-	Clay_Raylib_Initialize(800, 600, "Prosody", FLAG_WINDOW_RESIZABLE);
+	Clay_Initialize(clayMemory, (Clay_Dimensions) { .width = GetScreenWidth(), .height = GetScreenHeight() },  (Clay_ErrorHandler) { HandleClayErrors });
+	Clay_Raylib_Initialize(800, 600, "Prosody Clay", FLAG_WINDOW_RESIZABLE);
 	
 	Font fonts[1];
-    fonts[FONT_ID_BODY_16] = LoadFontEx("./resources/Roboto-Regular.ttf", 48, 0, 400);
-    SetTextureFilter(fonts[FONT_ID_BODY_16].texture, TEXTURE_FILTER_BILINEAR);
+	fonts[FONT_ID_BODY_16] = LoadFontEx("resources/Roboto-Regular.ttf", 48, 0, 400);
+	SetTextureFilter(fonts[FONT_ID_BODY_16].texture, TEXTURE_FILTER_BILINEAR);
 
 	while (!WindowShouldClose()) {
+        Clay_SetLayoutDimensions((Clay_Dimensions) {
+            .width = GetScreenWidth(),
+            .height = GetScreenHeight() / 2
+        });
+        
 		Clay_BeginLayout();
 		
 		CLAY({ .id = CLAY_ID(" body"),
